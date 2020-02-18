de41df1bd6873eecc5b44eec37e020e6016c5d25
Fect <xiazhongyujy@163.com>
2020-02-18 14:36:51 +0800

    930

---
+++ t930. Binary Subarrays With Sum.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int numSubarraysWithSum(vector<int>& A, int S) {
+
+    int n = A.size();
+    vector<int> dp(n+1,0);
+    int curn = 0;
+    int res = 0;
+    unordered_map<int,int> m;
+    m[0] = 1;
+    for(int i=0;i<A.size();i++){
+        curn+=A[i];
+        dp[i+1] = curn;
+        if(m.find(dp[i+1]-S)!=m.end()){
+            res+=m[dp[i+1]-S];
+        }    
+        m[dp[i+1]]++;
+    }
+    return res;
+}
+
+int main(){
+    vector<int> dp{0,0,0,0,0};
+    int res = numSubarraysWithSum(dp,0);
+    system("pause");
+}
