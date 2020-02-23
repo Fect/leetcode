b38856f8b6aa85bb60a6edc12dac19d732ce2506
Fect <xiazhongyujy@163.com>
2020-02-23 15:38:56 +0800

    1078

--- src/main/java/t1078OccurrencesAfterBigram.java
+++ src/main/java/t1078OccurrencesAfterBigram.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class t1078OccurrencesAfterBigram {
+
+    public String[] findOcurrences(String text, String first, String second) {
+        List<String> result = new ArrayList<String>();
+        String[] arr = text.split(" ");
+        for (int i=0; i<arr.length-2; i++) {
+            if (first.equals(arr[i]) && second.equals(arr[i+1])) {
+                result.add(arr[i+2]);
+            }
+        }
+        return result.toArray(new String[result.size()]);
+    }
+
 }
