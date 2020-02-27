2c21674381b087ab21a238598e90ea5c512b8812
Fect <xiazhongyujy@163.com>
2020-02-28 00:15:08 +0800

    242

---
+++ t242. Valid Anagram.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isAnagram(string s, string t) {
+    unordered_map<char,int> map1;
+    unordered_map<char,int> map2;
+
+    if(s.size()!=t.size()){
+        return false;
+    }
+    for(int i=0;i<s.size();i++){
+        map1[s[i]]++;
+        map2[t[i]]++;
+    }        
+    
+    return map1==map2;
+}
