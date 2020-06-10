b8a2f8306fe8380d881e64f5bfce11ccff39fbaf
Fect <xiazhongyujy@163.com>
2020-06-10 13:32:47 +0800

    2020.06.10

---
+++ t1351. Count Negative Numbers in a Sorted Matrix.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countNegatives(vector<vector<int>>& grid) {
+
+    int n = grid.size();
+    int m = grid[0].size();
+    
+
+}
+
+int main(){
+    system("pause");
+    return 0;
+}
