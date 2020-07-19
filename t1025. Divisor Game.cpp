f477e9800b3097b8815a0481bd6cd2790c82afb3
Fect <xiazhongyujy@163.com>
2020-07-19 19:19:01 +0800

    1025

--- t1025. Divisor Game.cpp
+++ t1025. Divisor Game.cpp
@@ -2,18 +2,31 @@
 using namespace std;
 
 bool divisorGame(int N) {
+
     vector<int> dp(N+1,0);
-    dp[1] = 1;
+
+    dp[1] = 0;
     bool flag = false;
-    for(int i=2;i<=N;i++){
-        for(int j=1;j<i;j++){
+    for(int i=2;i<N+1;i++){
+        for(int j=i-1;j>=1;j--){
             if(i%j==0){
                 if(dp[i-j]==0){
                     dp[i] = 1;
+                    flag = true;
+                    break;
                 }
             }
         }
+        if(!flag){
+            dp[i] = 0;
+        }
     }
     return dp[N];
+}
 
+int main(){
+    int res = divisorGame(2);
+    cout<<res<<endl;
+    system("pause");
+    return 0;
 }
