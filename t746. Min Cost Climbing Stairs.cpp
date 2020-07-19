29036891f5d5e39563c6e61cd1aa36ce18f69fe7
Fect <xiazhongyujy@163.com>
2020-07-19 23:14:35 +0800

    746

--- t746. Min Cost Climbing Stairs.cpp
+++ t746. Min Cost Climbing Stairs.cpp
@@ -4,12 +4,23 @@ using namespace std;
 int minCostClimbingStairs(vector<int>& cost) {
 
     int n = cost.size();
-    vector<int> dp(n+1,0);
-    dp[0] = cost[0];
-    dp[1] = min(cost[1],cost[0]);
+    vector<int> dp(n+1,0);    
 
-    for(int i=2;i<cost.size();i++){
-        dp[i] = min(dp[i-1]+cost[i],dp[i-2]+cost[i]);
+    dp[0] = cost[0];
+    dp[1] = cost[1];
+
+    for(int i=2;i<n;i++){
+        int tem = min(dp[i-1]+cost[i],dp[i-2]+cost[i]);
+        dp[i] = tem;
     }
-    return dp[n];
+    
+    return min(dp[n-1],dp[n-2]);
+}
+
+int main(){
+    vector<int> dp{0,0,1,1};
+    int res = minCostClimbingStairs(dp);
+    cout<<res<<endl;
+    system("pause");
+    return 0;
 }
