e95a57f4e17355f6d5558cf36d126912c3a4bea4
Fect <xiazhongyujy@163.com>
2020-07-19 22:57:03 +0800

    121

--- t121. Best Time to Buy and Sell Stock.cpp
+++ t121. Best Time to Buy and Sell Stock.cpp
@@ -3,15 +3,27 @@ using namespace std;
 
 int maxProfit(vector<int>& prices) {
 
+    int pre = INT_MAX;
+
     int res = 0;
-    int start = prices[0];
-    for(int i=1;i<prices.size();i++){
-        if(prices[i]<start){
-            start = prices[i];
-        }else{
-            res = min(res,prices[i]-start);
+
+    for(int i=0;i<prices.size();i++){
+        if(prices[i]<pre){
+            pre = prices[i];
+        }
+
+        if(prices[i]>pre){
+            res = max(res,prices[i]-pre);
         }
     }
 
     return res;
+}
+
+int main(){
+    vector<int> dp{7,1,5,3,6,4};
+    int res = maxProfit(dp);
+    cout<<res<<endl;
+    system("pause");
+    return 0;
 }
