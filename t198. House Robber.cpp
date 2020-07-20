859cba9fbc2fce4b5edcab0ede2da6be5800a0a1
Fect <xiazhongyujy@163.com>
2020-07-20 16:58:02 +0800

    198

--- t198. House Robber.cpp
+++ t198. House Robber.cpp
@@ -3,23 +3,15 @@ using namespace std;
 
 int rob(vector<int>& nums) {
     int n = nums.size();
-    
     if(n<=0){
         return 0;
     }
-    
     if(n==1){
         return nums[0];
     }
-    
-    vector<int> dp(n,0);
-    
-    dp[0]= nums[0];
-    dp[1] = max(nums[0],nums[1]);
-    //int res = dp[1];
-    for(int i=2;i<nums.size();i++){
-        dp[i] = max(dp[i-2]+nums[i],dp[i-1]);
-        //res = max(res,dp[i]);
+    vector<int> dp{nums[0],max(nums[0],nums[1])};
+    for(int i=2;i<n;i++){
+        dp.push_back(max(dp[i-1],dp[i-2]+nums[i]));
     }
 
     return dp.back();
