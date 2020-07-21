98f6e89f713cfb3ce0144aff4fed495ecc226cac
Fect <xiazhongyujy@163.com>
2020-07-22 06:26:46 +0800

    1277

--- t1277. Count Square Submatrices with All Ones.cpp
+++ t1277. Count Square Submatrices with All Ones.cpp
@@ -3,22 +3,21 @@ using namespace std;
 
 int countSquares(vector<vector<int>>& matrix) {
 
+    int m = matrix.size();
+    int n = matrix[0].size();
+
     int res = 0;
-    int n = matrix.size();
-    int m = matrix[0].size();
-
-    vector<vector<int>> dp(n,vector<int>(m,0));
-
-    for(int i=0;i<n;i++){
-        for(int j=0;j<m;j++){
+    vector<vector<int>> dp(m,vector<int>(n,0));
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(matrix[i][j]==0)
+                continue;
             if(i==0||j==0){
                 dp[i][j] = matrix[i][j];
             }else{
-                if(matrix[i][j]==1){
-                    dp[i][j] = min(dp[i-1][j-1],min(dp[i][j-1],dp[i-1][j]))+1;
-                }
+                dp[i][j] = min(dp[i-1][j-1],min(dp[i][j-1],dp[i-1][j]))+1;
             }
-            res+=dp[i][j];
+                res+=dp[i][j];
         }
     }
     return res;
