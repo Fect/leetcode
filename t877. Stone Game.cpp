7958c045209cfbbd9c7ad886768b74636d1d9c14
Fect <xiazhongyujy@163.com>
2020-07-22 14:17:25 +0800

    877

--- t877. Stone Game.cpp
+++ t877. Stone Game.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 bool stoneGame(vector<int>& piles) {
 
-    int n = piles.size();
-    vector<vector<int>> dp(n,vector<int>(n,0));
+    int m = piles.size();
+    vector<vector<int>> dp(m,vector<int>(m,0));
 
-    for(int i=0;i<n;i++){
+    for(int i=0;i<m;i++){
         dp[i][i] = piles[i];
-        for(int j=0;j<i;j++){
+        for(int j=i-1;j>=0;j--){
             dp[j][i] = max(piles[j]-dp[j+1][i],piles[i]-dp[j][i-1]);
         }
     }
-    return dp[0][n-1]>0;
+    return dp[0][m-1]>0;
 }
