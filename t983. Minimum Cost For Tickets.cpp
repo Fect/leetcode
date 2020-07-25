f522c0bed06e839c0a4eca427117f1c5254b7854
Fect <xiazhongyujy@163.com>
2020-07-26 04:48:34 +0800

    983

---
+++ t983. Minimum Cost For Tickets.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int mincostTickets(vector<int>& days, vector<int>& costs) {
+    vector<int> dp(366,0);
+    for(int i=1;i<366;i++){
+        if(find(days.begin(),days.end(),i)==days.end()){
+            dp[i] = dp[i-1];
+        }else{
+            dp[i] = min(dp[max(0,i-1)]+costs[0],min(dp[max(0,i-7)]+costs[1],dp[max(0,i-30)]+costs[2]));
+        }
+    }
+
+    return dp[365];
+}
