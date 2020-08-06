d0c690994568bada8fb4cd1aacd4891ee14c72d2
Fect <xiazhongyujy@163.com>
2020-08-06 22:18:57 +0800

    714

--- t1372. Longest ZigZag Path in a Binary Tree.cpp
+++ t1372. Longest ZigZag Path in a Binary Tree.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
  struct TreeNode {
@@ -10,25 +10,26 @@ using namespace std;
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-int maxStep = 0;
+int maxDep = 0;
 
-void dfs(TreeNode* root,bool isLeft,int step){
+void dfs(TreeNode* root,bool isLeft,int depth){
+
     if(!root){
-        return;
+        return 0;
     }
-    maxStep = max(maxStep,step);
+    maxDep = max(maxDep,depth);
     if(isLeft){
-        dfs(root->left,false,step+1);
+        dfs(root->left,false,depth+1);
         dfs(root->right,true,1);
     }else{
-        dfs(root->right,true,step+1);
+        dfs(root->right,true,depth+1);
         dfs(root->left,false,1);
     }
 }
 
-int longestZigZag(TreeNode* root){
+int longestZigZag(TreeNode* root) {
     dfs(root,true,0);
     dfs(root,false,0);
 
-    return maxStep;
+    return maxDep;
 }
