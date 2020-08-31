5cc8064088ebba0c91f77ba40502d879e35dee9a
Fect <xiazhongyujy@163.com>
2020-08-31 21:33:04 +0800

    commit

--- src/main/java/t120/Triangle.java
+++ src/main/java/t120/Triangle.java
@@ -1,4 +1,53 @@
 package t120;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Triangle {
+
+    public static int minimumTotal(List<List<Integer>> triangle) {
+
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+
+        for(int i=0;i<triangle.get(0).size();i++){
+            dp[0][i] = triangle.get(0).get(i);
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<triangle.get(i).size();j++){
+                if(j<triangle.get(i-1).size()){
+                    dp[i][j] = Math.min(dp[i-1][j]+triangle.get(i).get(j),dp[i-1][j-1>=0?j-1:0]+triangle.get(i).get(j));
+                }else{
+                    dp[i][j] = dp[i-1][j-1>=0?j-1:0]+triangle.get(i).get(j);
+                }
+            }
+        }
+        int res = dp[n-1][0];
+        for(int i=1;i<triangle.get(n-1).size();i++){
+            res = Math.min(res,dp[n-1][i]);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args){
+        List<List<Integer>> dp = new ArrayList<List<Integer>>();
+
+        List<Integer> L1 = new ArrayList<Integer>();
+        List<Integer> L2 = new ArrayList<Integer>();
+        List<Integer> L3 = new ArrayList<Integer>();
+        List<Integer> L4 = new ArrayList<Integer>();
+
+        L1.add(2);
+        dp.add(L1);
+        L2.add(3); L2.add(4);
+        dp.add(L2);
+        L3.add(6);L3.add(5);L3.add(7);
+        dp.add(L3);
+        L4.add(4); L4.add(1); L4.add(8); L4.add(3);
+        dp.add(L4);
+
+        int res = minimumTotal(dp);
+    }
 }
--- src/main/java/t1218/LongestArithmeticSubsequenceofGivenDifference.java
+++ src/main/java/t1218/LongestArithmeticSubsequenceofGivenDifference.java
@@ -1,4 +1,26 @@
 package t1218;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestArithmeticSubsequenceofGivenDifference {
+
+    public int longestSubsequence(int[] arr, int difference) {
+
+        int n = arr.length;
+        int res = 0;
+
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i=0;i<n;i++){
+            if(map.containsKey(arr[i]-difference)){
+                map.put(arr[i],Math.max(map.containsKey(arr[i])?map.get(arr[i]):0,map.get(arr[i]-difference)+1));
+            }else{
+                map.put(arr[i],1);
+            }
+            res = Math.max(map.get(arr[i]),res);
+        }
+
+
+        return res;
+    }
 }
